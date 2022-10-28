c736323e474d7b850ca5148a5219800f3311420a
Na Lin <lna483018@gmail.com>
2022-10-27 20:45:12 -0400

    connect mongodb with express by using mongoose

--- server.js
+++ server.js
@@ -1,3 +1,25 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config({ path: './config.env' });
+// console.log(process.env);
+
+// connect mongodb with express
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then((con) => {
+    console.log(con.connections);
+    console.log('DB connect successful');
+  });
+
 const app = require('./app');
 
 const port = 3000;
